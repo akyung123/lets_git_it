46896fffe6e6ba3cff9bc3bfeab8cbd318116692
akyoung123 <2004924@donga.ac.kr>
2025-06-27 16:22:24 +0900

    login screen 구현 중

---
+++ my_app/lib/elder/screens/login_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController idController = TextEditingController();
+
+  bool isLoading = false; // 로딩 상태 수
+
+  Future<void> loginUser() async {
+    String id = idController.text.trim();
+    
+    if (id.isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('인식번호(id)를 입력하세요요')),
+      );
+      return ;
+    }
+
+    setState(() {
+      isLoading = true; // 로딩 시작
+    });
+
+    // login 관련 DB 확인 후, 인식번호로 로그인
+    try {
+      UserCredential userCredential =
+        await
+    }
+    DocumentSnapshot userDoc = (await FirebaseFirestore.instance
+        .collection('users')
+        .get()) as DocumentSnapshot<Object?>;
+
+    if (userDoc.exists) {
+      Map<
+    }
+  
+
+}
