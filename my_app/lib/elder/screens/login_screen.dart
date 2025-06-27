881bbb05fd64d2bc8aec9955d42b77c11f4e409a
akyoung123 <2004924@donga.ac.kr>
2025-06-27 18:09:16 +0900

    login elder screen 구현

--- my_app/lib/elder/screens/login_screen.dart
+++ my_app/lib/elder/screens/login_screen.dart
@@ -18,7 +18,7 @@ class _LoginScreenState extends State<LoginScreen> {
     
     if (id.isEmpty){
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('인식번호(id)를 입력하세요요')),
+        const SnackBar(content: Text('인식번호(id)를 입력하세요')),
       );
       return ;
     }
@@ -37,9 +37,54 @@ class _LoginScreenState extends State<LoginScreen> {
         
       if (QuerySnapshot.docs.isNotEmpty) {
           final userData = QuerySnapshot.docs.first.data();
-          
+          final userId = QuerySnapshot.docs.first.id;
+
+          // 로그인 성공 처리
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('${userData['name']}님 환영합니다!')),
+          );
+
+          // Home_screen으로 이동시켜야함
+          //Navigator.push(context, MaterialPageRoute(builder: (_) => Home_screen(userId: userId))); 
+        } else {
+          // id가 없음
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('해당 인식 번호가 존재하지않습)니다.')),
+          );
         }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('로그인 중 오류 발생: $e')),
+      );
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
-
-
+  }
+  
+  @override
+    build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('로그인')),
+        body : Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: idController,
+                decoration: const InputDecoration(
+                  labelText: '인식번호 입력',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(onPressed: isLoading ? null : loginUser,
+              child: isLoading ? const CircularProgressIndicator()
+              : const Text('로그인'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
